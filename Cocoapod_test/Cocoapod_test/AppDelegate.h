f06fe3ce8c7f18990bf790e8d34e7ed98524fc3f
张泽 <zhangzeStrong@126.com>
2017-04-26 15:07:19 +0800

    项目初始化

---
+++ Cocoapod_test/Cocoapod_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cocoapod_test
+//
+//  Created by yaoln on 2017/4/26.
+//  Copyright © 2017年 zhangze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
